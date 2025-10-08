e06b4287e89cd6e838c83a61c11b5aad5bc50014
javadrad <43435138+javadrad@users.noreply.github.com>
2025-10-08 13:58:44 +0330

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,42 +1,19 @@
-const CACHE_NAME = "tool-inventory-cache-v1";
-const urlsToCache = [
-  "/",
-  "/static/style.css",
-  "/static/fonts/B-NAZANIN.TTF",
-  "/static/icons/icon-192.png",
-  "/static/icons/icon-512.png",
-  "/static/manifest.json"
-];
-
-// نصب Service Worker
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('app-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/static/style.css',
+        '/static/manifest.json'
+      ]);
     })
   );
 });
 
-// فعال‌سازی و پاکسازی کش‌های قدیمی
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-});
-
-// واکشی فایل‌ها از کش در صورت آفلاین بودن
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
   );
 });
